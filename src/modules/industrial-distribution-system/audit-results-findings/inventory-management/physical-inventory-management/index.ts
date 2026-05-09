643739b14310d3973406537895b64d72a3affe09
Erwinrillorta0830 <erwin.rillort.0330@gmail.com>
2026-05-09 16:23:16 +0800

    add physical inventory

---
+++ src/modules/industrial-distribution-system/audit-results-findings/inventory-management/physical-inventory-management/index.ts
@@ -0,0 +1,8 @@
+//src/modules/supply-chain-management/physical-inventory-management/index.ts
+export * from "./types";
+export * from "./providers/fetchProvider";
+export * from "./utils/compute";
+export * from "./utils/grouping";
+export * from "./utils/printAuditSheet";
+export * from "./components";
+export * from "./PhysicalInventoryManagementModule";
