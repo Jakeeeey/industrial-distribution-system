8647b69f25ef07d5d92630d05e3fcc718d33d1ca
Erwinrillorta0830 <erwin.rillorta.0330@gmail.com>
2026-05-09 16:32:08 +0800

    Erwin branch (#4)
    * update ids again
    
    * add physical inventory
    
    ---------
    
    Co-authored-by: Erwinrillorta0830 <erwin.rillort.0330@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['msi-andrie', 'msi-jake', '100.81.225.79', '100.124.104.46', 'msi-eulysis'],
+  allowedDevOrigins: ['msi-andrie', 'msi-jake', '100.81.225.79', '100.124.104.46', 'msi-eulysis', "100.119.3.44"],
 };
 
 export default nextConfig;
